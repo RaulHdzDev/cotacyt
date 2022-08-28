edc512f9787fe5fb908d410ae5d7c8fb1983ddbe
RaulHdzDev <rahdz.0799@gmail.com>
2022-08-28 01:20:54 -0500

    excel y rama 2.4

--- src/app/config/services.config.ts
+++ src/app/config/services.config.ts
@@ -4,9 +4,5 @@ export class ServicesConfig {
     // APP_ENDPOINT = 'http://plataforma.cotacyt.gob.mx/API_COTACYT/public/';
 
     //APP_ENDPOINT_LOCAL2 = 'http://localhost:8000/api';
-<<<<<<< HEAD
      APP_ENDPOINT_LOCAL2 = 'http://plataforma.cotacyt.gob.mx/apiCecit2022/public/api';
-=======
-    APP_ENDPOINT_LOCAL2 = 'http://plataforma.cotacyt.gob.mx/apiCecit2022/public/api';
->>>>>>> 79e65b443c1f0ae8969d9969f841038a330fd229
 }
