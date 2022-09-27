772452249887ebfdf2d5b22dcb43274e4bb607a2
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2022-09-27 15:59:22 -0500

    prod

--- src/app/config/services.config.ts
+++ src/app/config/services.config.ts
@@ -1,9 +1,9 @@
 export class ServicesConfig {
     //  APP_ENDPOINT = 'http://localhost:8081/php/API-COTACYT/public/';
-    APP_ENDPOINT = 'https://mante.hosting.acm.org/API-COTACYT/public/';
-    // APP_ENDPOINT = 'http://plataforma.cotacyt.gob.mx/API-COTACYT/public/';
+    // APP_ENDPOINT = 'https://mante.hosting.acm.org/API-COTACYT/public/';
+    APP_ENDPOINT = 'http://plataforma.cotacyt.gob.mx/API-COTACYT/public/';
     // APP_ENDPOINT = 'http://plataforma.cotacyt.gob.mx/API_COTACYT/public/';
 
-    APP_ENDPOINT_LOCAL2 = 'http://localhost:8000/api';
-    //  APP_ENDPOINT_LOCAL2 = 'http://plataforma.cotacyt.gob.mx/apiCecit2022/public/api';
+    //APP_ENDPOINT_LOCAL2 = 'http://localhost:8000/api';
+     APP_ENDPOINT_LOCAL2 = 'http://plataforma.cotacyt.gob.mx/apiCecit2022/public/api';
 }
